5a348cf475c4a2df259eae249cc0a179bc48a577
Will Potter <will@wpotter.me>
2013-08-11 17:33:48 -0400

    config stuff

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,14 @@
-keys = YAML.load(File.open("secret/keys.yml"))
+if ENV["RAILS_ENV"] == "development"
+  keys = YAML.load(File.open("secret/keys.yml"))
+else
+
+  keys = {}
+  keys[:password_salt] = ENV["PASSWORD_SALT"]
+  keys[:twitter_consumer_key] = ENV["TWITTER_CONSUMER_KEY"]
+  keys[:twitter_consumer_secret] = ENV["TWITTER_CONSUMER_SECRET"]
+  keys[:twitter_oauth_token] = ENV["TWITTER_OAUTH_TOKEN"]
+  keys[:twitter_oauth_token_secret] = ENV["TWITTER_OAUTH_TOKEN_SECRET"]
+end
 
 Twitter.configure do |config|
   config.consumer_key = keys["twitter_consumer_key"]
