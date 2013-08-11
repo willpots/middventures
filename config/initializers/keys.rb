3189f8d30b6efbb86c1f2e13c2e9287f7bd0d212
Will Potter <will@wpotter.me>
2013-08-11 17:31:45 -0400

    fixing a short config bug

--- config/initializers/keys.rb
+++ config/initializers/keys.rb
@@ -2,7 +2,7 @@ if ENV["RAILS_ENV"] == "development"
   SECRET_CONFIG = YAML.load(File.open("secret/keys.yml"))
 else
 
-  SECRET_CONFIG = []
+  SECRET_CONFIG = {}
   SECRET_CONFIG[:password_salt] = ENV["PASSWORD_SALT"]
   SECRET_CONFIG[:twitter_consumer_key] = ENV["TWITTER_CONSUMER_KEY"]
   SECRET_CONFIG[:twitter_consumer_secret] = ENV["TWITTER_CONSUMER_SECRET"]
